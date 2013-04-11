3c09cfa3d006ed8e45cb536338335cb61e612e1c
Applied-Duality <emeijer@applied-duality.com>
2013-04-11 05:11:42 -0700

    xRx first commit

---
+++ ExtensionsForReactiveExtensions/xRx.cs
@@ -0,0 +1,30 @@
+﻿#region Apache 2 License
+// Copyright (c) Applied Duality, Inc., All rights reserved.
+// See License.txt in the project root for license information.
+#endregion
+
+using System.Reactive.Concurrency;
+
+namespace System.Reactive.Linq
+{
+    public static class xRx
+    {
+        /// <summary>
+        /// Convert scheduler to observable.
+        /// </summary>
+        public static IObservable<Unit> ToObservable(this IScheduler scheduler)
+        {
+            return Observable.Create<Unit>(observer =>
+            {
+                return scheduler.ScheduleAsync(async (_scheduler, token) =>
+                {
+                    while (!token.IsCancellationRequested)
+                    {
+                        observer.OnNext(Unit.Default);
+                        await _scheduler.Yield();
+                    }
+                });
+            });
+        }
+    }
+}
---
+++ xRx/Program.cs
@@ -0,0 +1,34 @@
+﻿#region Apache 2 License
+// Copyright (c) Applied Duality, Inc., All rights reserved.
+// See License.txt in the project root for license information.
+#endregion
+
+using System;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+
+namespace xRx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var xs = Observable.Defer(delegate
+            {
+                var i = 0;
+                return from _ in ThreadPoolScheduler.Instance.ToObservable()
+                       select i++;
+            });
+
+            var dispose = xs.Subscribe(Console.WriteLine);
+
+            Console.WriteLine("Press any key to unsubscribe ...");
+            Console.ReadKey();
+
+            dispose.Dispose();
+
+            Console.ReadKey();
+            Console.WriteLine("Bye!");
+        }
+    }
+}
